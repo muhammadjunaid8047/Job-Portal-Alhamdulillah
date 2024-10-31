2bb19120117ce60bae7b6962d438108a6c613f47
Muhammad Junaid <muhammadjunaid8047@gmail.com>
2024-10-31 05:02:14 +0500

    ..

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -79,7 +79,7 @@ export default function FooterCom() {
             <Footer.Icon href='#' icon={BsFacebook}/>
             {/* <Footer.Icon href='#' icon={BsInstagram}/> */}
             <Footer.Icon href='#' icon={BsLinkedin}/>
-            {/* <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/> */}
+            {/* <Footer.Icon href=' icon={BsGithub}/> */}
             <Footer.Icon href='#' icon={BsWhatsapp}/>
 
           </div>

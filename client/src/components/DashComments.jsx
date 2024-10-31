7f9c37d13545a94854a356dd37be94ef6f50ce8f
Muhammad Junaid <muhammadjunaid8047@gmail.com>
2024-10-31 05:15:00 +0500

    comment section

--- client/src/components/DashComments.jsx
+++ client/src/components/DashComments.jsx
@@ -78,9 +78,9 @@ export default function DashComments() {
           <Table hoverable className='shadow-md'>
             <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
-              <Table.HeadCell>Comment content</Table.HeadCell>
+              <Table.HeadCell>Question content</Table.HeadCell>
               <Table.HeadCell>Number of likes</Table.HeadCell>
-              <Table.HeadCell>PostId</Table.HeadCell>
+              <Table.HeadCell>InternshipId</Table.HeadCell>
               <Table.HeadCell>UserId</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
             </Table.Head>
@@ -132,7 +132,7 @@ export default function DashComments() {
           <div className='text-center'>
             <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
             <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
-              Are you sure you want to delete this comment?
+              Are you sure you want to delete this question?
             </h3>
             <div className='flex justify-center gap-4'>
               <Button color='failure' onClick={handleDeleteComment}>
